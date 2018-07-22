45245002e402d350f74c2ffda69d8dd7763e96e7
louismn <louis.martindunord@eemi.com>
2018-07-22 18:48:01 +0200

    Ajout des exercices de Jeudi + réponses aux questions

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Cette commande récupere le texte écrit par l'internaute et les inseres dans une variable" 
+
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts "Quel est ton prénom ?"
+user_name = gets.chomp
+puts "Quel est ton nom ?"
+user_lastname = gets.chomp
+puts "Bonjour #{user_name} #{user_lastname}!"
